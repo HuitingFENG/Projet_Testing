751d8175917c6a805a629e765927390ec4599063
FENG <38035703+HuitingFENG@users.noreply.github.com>
2024-01-24 12:15:54 +0100

    update

--- secondTest.py
+++ secondTest.py
@@ -43,17 +43,18 @@ def test_remove_manager_role(browser_context):
     page.goto('https://i.hr.dmerej.info/')
     page.get_by_role('link', name='List Employees').click()
     # check an old employee who is not a manager yet
-    page.get_by_role('button', name='Edit').click()
+    page.get_by_role('link', name='Edit').click(timeout=60000) 
     page.get_by_role('link', name='Promote as manager').click()
     page.get_by_role('button', name='Proceed').click()
     page.get_by_role('link', name='Home').click()
     page.get_by_role('link', name='List Employees').click()
-    page.get_by_role('button', name='Edit').click()
+    page.get_by_role('link', name='Edit').click()
     page.get_by_role('link', name='Promote as manager').click()
     # Check if any new element is added after promoting as manager
     new_elements = page.locator('*')  # Selects all elements on the page
     assert new_elements.count() > 0  # Check if there's at least one new element: button called remove
     context.tracing.stop(path='trace.json')
+    
     
 def test_zipcode(browser_context):
     context = browser_context
@@ -61,7 +62,7 @@ def test_zipcode(browser_context):
     context.tracing.start()
     page.goto('https://i.hr.dmerej.info/')
     page.get_by_role('link', name='List Employees').click()
-    page.get_by_role('button', name='Edit').click()
+    page.get_by_role('link', name='Edit').click()
     page.get_by_role('link', name='Update address').click()
     page.get_by_placeholder('Zip code').click()
     page.press('Zip code', 'Enter') 
