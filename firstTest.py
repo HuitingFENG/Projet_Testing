3e04df21ff5210da02effef18ddf33995b3c1810
Senhua <senhua.liu@efrei.net>
2024-01-24 10:46:35 +0100

    test add employee

--- firstTest.py
+++ firstTest.py
@@ -8,6 +8,14 @@ def test_website_link():
     
 def test_add_employee():
     url = "https://i.hr.dmerej.info/add_employee"
-    data = {"name": "Test Employee"}
+    data = {
+        "name": "Jean",
+        "email": "test.employee@example.com",
+        "address": "123 rue de la rue",
+        "city": "City",
+        "zipcode": "12345",
+        "hiring_date": "01/01/2020",
+        "job_title": "RH"
+    }
     response = requests.post(url, data=data)
     assert response.status_code == 200, "Failed to add employee"
