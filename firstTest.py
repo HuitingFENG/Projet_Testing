9b4a41a37a1d75ff3380a858f3aff50222295ba0
Senhua <senhua.liu@efrei.net>
2024-01-24 11:06:16 +0100

    rebase

--- firstTest.py
+++ firstTest.py
@@ -1,23 +1,4 @@
 import pytest
-<<<<<<< HEAD
-import requests
-from playwright.sync_api import sync_playwright
-
-
-def test_website_link():
-    url = "https://i.hr.dmerej.info/"
-    response = requests.get(url)
-    assert response.status_code == 200, "Website is not accessible"
-    
-def test_add_employee():
-    url = "https://i.hr.dmerej.info/add_employee"
-    data = {"name": "Test Employee"}
-    response = requests.post(url, data=data)
-    assert response.status_code == 200, "Failed to add employee"
-    
-    
-    
-=======
 from playwright.sync_api import sync_playwright
 
 def test_firefox():
@@ -26,4 +7,3 @@ def test_firefox():
         page = browser.new_page()
         page.goto("https://i.hr.dmerej.info/add_employee")  # replace with your website link
         browser.close()
->>>>>>> 666b030ec18643c07b31760db2ff4e0104531913
