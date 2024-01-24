1126a255507a6da36ff691a266c333ec426b2705
FENG <38035703+HuitingFENG@users.noreply.github.com>
2024-01-24 12:00:14 +0100

    update

--- secondTest.py
+++ secondTest.py
@@ -34,19 +34,7 @@ def test_add_new_employee(browser_context):
     context.tracing.stop(path='trace.json')
 
 
-# reset db without password
-def test_reset_db_without_password (browser_context):
-    context = browser_context
-    page = context.new_page()
-    context.tracing.start()
-    page.goto('https://i.hr.dmerej.info/')
-    page.get_by_role('link', name='Reset database').click()
-    page.get_by_role('button', name='Proceed').click()
-     # Check if the user is back on the main page after reset
-     # user can go back to the main page, means that the database can be reset without password.
-    assert page.url == 'https://i.hr.dmerej.info/'
-    context.tracing.stop(path='trace.json')
-    
+
     
 def test_remove_manager_role(browser_context):
     context = browser_context
@@ -66,3 +54,42 @@ def test_remove_manager_role(browser_context):
     new_elements = page.locator('*')  # Selects all elements on the page
     assert new_elements.count() > 0  # Check if there's at least one new element: button called remove
     context.tracing.stop(path='trace.json')
+    
+def test_zipcode(browser_context):
+    context = browser_context
+    page = context.new_page()
+    context.tracing.start()
+    page.goto('https://i.hr.dmerej.info/')
+    page.get_by_role('link', name='List Employees').click()
+    page.get_by_role('button', name='Edit').click()
+    page.get_by_role('link', name='Update address').click()
+    page.get_by_placeholder('Zip code').click()
+    page.press('Zip code', 'Enter') 
+    page.get_by_placeholder('Zip code').fill('11111111111111111111111111111111111111111111111111111111')
+    page.get_by_role('button', name='Update').click()
+    # Wait for the new page to load
+    new_page = page.wait_for_event('page', 'popup')
+    # Assert that the new page contains "server code 500"
+    new_page_content = new_page.content()
+    assert "server code 500" in new_page_content, "Expected 'server code 500' on the new page"
+    
+    #  can also assert the status code if needed
+    # assert new_page.status() == 500, "Expected status code 500 on the new page"
+    context.tracing.stop(path='trace.json')
+    
+    
+    
+    
+# reset db without password
+def test_reset_db_without_password (browser_context):
+    context = browser_context
+    page = context.new_page()
+    context.tracing.start()
+    page.goto('https://i.hr.dmerej.info/')
+    page.get_by_role('link', name='Reset database').click()
+    page.get_by_role('button', name='Proceed').click()
+     # Check if the user is back on the main page after reset
+     # user can go back to the main page, means that the database can be reset without password.
+    assert page.url == 'https://i.hr.dmerej.info/'
+    context.tracing.stop(path='trace.json')
+    
